9720da4ed20e6520bc288e76c97e1ad8ec06cc86
Dean Erickson <dmepilot@yahoo.com>
2020-07-08 14:35:34 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
@@ -8,6 +9,7 @@ class Scraper
   def get_page
     
   end
+  
   def print_courses
     self.make_courses
     Course.all.each do |course|
